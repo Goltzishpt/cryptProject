235104f859f5e0a3d621cff52f87406d5193c901
Goltz <95189224+Goltzishpt@users.noreply.github.com>
2023-01-10 05:25:28 +0200

    added patch, put, delete and exceptions

--- cryptProject/main.py
+++ cryptProject/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel, Field
 from typing import Optional
 from datetime import date
@@ -22,11 +22,25 @@ class User(BaseModel):
     age: int = Field(None, gt=5, lt=130)
 
 
+class UserUpdate(User):
+    date_joined: Optional[date] = None
+    age: int = Field(None, gt=5, lt=200)
+
+
+def ensure_username_in_db(username: str):
+    """
+    if username not in database he has exception with code 404
+    """
+    if username not in user_db:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Username {username} not found.')
+
+
 app = FastAPI()
 
 
 @app.get('/users')
-def get_users_query(limit: int = 20):
+async def get_users_query(limit: int = 20):
     """
     This func shows all users
     """
@@ -35,18 +49,78 @@ def get_users_query(limit: int = 20):
 
 
 @app.get('/users/{username}')
-def get_users_path(username: str):
+async def get_users_path(username: str):
     """
     This func shows certain user from db
     """
+    if username not in user_db:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Username {username} not found.')
     return user_db[username]
 
 
 @app.post('/users')
-def create_user(user: User):
+async def create_user(user: User):
     """
     This func accept info from user and create user
     """
     username = user.username
+    ensure_username_in_db(username)
     user_db[username] = user.dict()
     return {'message': f'Successfully created user: {username}'}
+
+
+@app.delete('/users/{username}')
+async def delete_user(username: str):
+    """
+    this func delete user
+    """
+    ensure_username_in_db(username)
+    del user_db[username]
+    return {'message': f'Successfully deleted user: {username}'}
+
+
+@app.put('/users')
+async def update_user(user: User):
+    """
+    replace the ALL data by username
+    EVERYTHING IS CHANGING
+    """
+    username = user.username
+    ensure_username_in_db(username)
+    user_db[username] = user.dict()
+    return {'message': f'Successfully update user: {username}'}
+
+
+@app.patch('/users')
+async def update_user_partial(user: UserUpdate):
+    """
+    replace the PARTIAL data by username
+    PART IS CHANGING
+
+    exclude_unset for fields with value None(like an age)
+
+    >>user_db[username].update(user.dict(exclude_unset=True))
+    >>part change
+
+    >>user_db[username].update(user.dict())
+    >>without exclude_unset changes all
+
+    >>user_db[username] = (user.dict(exclude_unset=True))
+    >>deleted not changed fields
+    """
+    username = user.username
+    ensure_username_in_db(username)
+    user_db[username].update(user.dict(exclude_unset=True))
+    return {'message': f'Successfully update user: {username}'}
+
+
+# post does the same as patch above
+#
+# ??? can there be only one post ???
+#
+# @app.post('/users')
+# async def update_user_partial_post(user: User):
+#     username = user.username
+#     user_db[username].update(user.dict(exclude_unset=True))
+#     return {'message': f'Successfully update user: {username}'}
