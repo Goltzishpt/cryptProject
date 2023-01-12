12d22122c35f0acd2afea9948e91ecd8f69698b7
Goltz <95189224+Goltzishpt@users.noreply.github.com>
2023-01-12 03:47:29 +0200

    create csv
    added csv with average value

--- cryptProject/main.py
+++ cryptProject/main.py
@@ -1,5 +1,7 @@
 import requests
 import pandas as pd
+from datetime import datetime
+
 
 headers = {
     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
@@ -8,17 +10,21 @@ headers = {
 
 def parse_csv(coin_id, name):
     """
-    parse information about chart with the help of magic
-    turn into json file
-    get column
-    convert to csv
+    - parse information about chart with the help of magic
+    - turn into json file
+    - make list with dict(key = column, value = row), convert sec to datetime
+    - create dataframe
+    - calculate the average(first value rolling = quantity row with digits)
+    - convert to csv
     """
     url = f"https://api.coinmarketcap.com/data-api/v3/cryptocurrency/detail/chart?id={coin_id}&range=7D"
     r = requests.get(url, headers=headers).json()
-    df = pd.DataFrame(r['data'])
+    data_frame = [{'date': datetime.fromtimestamp(int(k)), 'value': v['v'][0]} for k, v in r['data']['points'].items()]
+    df = pd.DataFrame(data_frame)
+    df['avg_last_5'] = df.rolling(5, min_periods=1).value.mean()
     df.to_csv(f"{name}_coin_data.csv", encoding='utf-8', index=True)
 
 
-parse_csv(2010, 'cardano')
-
-parse_csv(11419, 'ton')
+if __name__ == '__main__':
+    parse_csv(2010, 'cardano')
+    # parse_csv(11419, 'ton')
